5ddf5917a57de8c0eab6e95cb69ec6edcade5ff1
yixianj <yixianj@umich.edu>
2020-04-08 16:50:27 -0400

    Updates

---
+++ precache-manifest.4d86e4b95b94b3d2408ef05721d64af1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f93a60987d58bb68ab4dca5d5495b1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73b7e9567d5110169fc7",
+    "url": "/static/css/main.8653d04c.chunk.css"
+  },
+  {
+    "revision": "c37b60a597c57983afe3",
+    "url": "/static/js/2.653af8aa.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.653af8aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73b7e9567d5110169fc7",
+    "url": "/static/js/main.4f5b6ac2.chunk.js"
+  },
+  {
+    "revision": "b7d19cf78f280fa66d2234df2807ea7c",
+    "url": "/static/js/main.4f5b6ac2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6490f87b713e93fd853",
+    "url": "/static/js/runtime-main.7c1a7ef6.js"
+  },
+  {
+    "revision": "33d84f71a5796556f0517b9e3c0694a1",
+    "url": "/static/media/gameOver.33d84f71.mp3"
+  },
+  {
+    "revision": "d829ece54126931caf0a13884a6a0a21",
+    "url": "/static/media/hail.d829ece5.mp3"
+  },
+  {
+    "revision": "d60a7f69f620cb891bd5d1237293d771",
+    "url": "/static/media/piano.d60a7f69.wav"
+  },
+  {
+    "revision": "640638fc4cd21bd3018076dbebbc3328",
+    "url": "/static/media/sad.640638fc.png"
+  },
+  {
+    "revision": "82532eefdffeba1f1e7d20d39ab64036",
+    "url": "/static/media/sadSquirrel.82532eef.png"
+  },
+  {
+    "revision": "802980480ec050bfbce370deccf0d5ba",
+    "url": "/static/media/umichWinter.80298048.jpg"
+  }
+]);
--- src/js/app/helpers/testingUtil.js
+++
@@ -1,7 +0,0 @@
-export function printVertices(vertices) {
-    console.log("### VERTICES ###");
-    for (let i = 0; i < vertices.length; i++) {
-        console.log(vertices[i].x + ", " + vertices[i].y);
-    }
-    console.log("#####");
-}
